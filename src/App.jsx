8b2b0bb6d1a62c1a4aaf939bfc21886359474125
AKKI <2300030796@kluniversity.in>
2025-10-15 23:48:17 +0530

    added txt

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My practice demo project of react</h1>
+      <p>--A simple homepage deployed on GitHub Actions using Vite.</p>
+      <h1>Done with Git Actions</h1>
+      <p>30796-SEC105</p>
+    </div>
+  );
+}
+export default App;
